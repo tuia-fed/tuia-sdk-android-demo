8004de79b3ff7eb0d948dbb4bb2b2e88d2ec344d
罗泽宇 <luozeyu@duiba.com.cn>
2020-04-23 20:57:39 +0800

    1.更新V2.3.0.x-1

--- demo/src/main/java/com/lechuan/midunovel1/demo/NativeInterstitialActivity.java
+++ demo/src/main/java/com/lechuan/midunovel1/demo/NativeInterstitialActivity.java
@@ -7,9 +7,11 @@ import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 
+import com.lechuan.midunovel.base.util.FoxBaseLogger;
 import com.lechuan.midunovel.demo.R;
 import com.lechuan.midunovel.nativead.Ad;
 import com.lechuan.midunovel.nativead.AdCallBack;
+import com.lechuan.midunovel.nativead.DefaultAdCallBack;
 
 public class NativeInterstitialActivity extends BaseActivity {
 
@@ -26,52 +28,52 @@ public class NativeInterstitialActivity extends BaseActivity {
             slotId = getIntent().getIntExtra("slotId", 0);
         }
         //s1：slotId  s2:userId  s3:deviceId
-        ad = new Ad("4UycwwZv41rwzne1ZXgtQBgDSnPH", slotId+"", userId, "");
-        ad.init(NativeInterstitialActivity.this, null, Ad.AD_URL_NEW, new AdCallBack() {
-
-            @Override
-            public void onReceiveAd() {
-                Log.d("========", "onReceiveAd");
-            }
-
-            @Override
-            public void onFailedToReceiveAd() {
-                Log.d("========", "onFailedToReceiveAd");
-            }
-
-            @Override
-            public void onActivityClose() {
-                Log.d("========", "onActivityClose");
-            }
-
-            @Override
-            public void onActivityShow() {
-                Log.d("========", "onActivityShow");
-            }
-
-            @Override
-            public void onRewardClose() {
-                Log.d("========", "onRewardClose");
-            }
-
-            @Override
-            public void onRewardShow() {
-                Log.d("========", "onRewardShow");
-            }
-
-            @Override
-            public void onPrizeClose() {
-                Log.d("========", "onPrizeClose");
-            }
-
-            @Override
-            public void onPrizeShow() {
-                Log.d("========", "onPrizeShow");
-            }
-        });
         findViewById(R.id.btn_req).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                ad = new Ad("4UycwwZv41rwzne1ZXgtQBgDSnPH", slotId+"", userId, "");
+                ad.init(NativeInterstitialActivity.this, null, Ad.AD_URL_NEW, new AdCallBack() {
+
+                    @Override
+                    public void onReceiveAd() {
+                        Log.d("========", "onReceiveAd");
+                    }
+
+                    @Override
+                    public void onFailedToReceiveAd() {
+                        Log.d("========", "onFailedToReceiveAd");
+                    }
+
+                    @Override
+                    public void onActivityClose() {
+                        Log.d("========", "onActivityClose");
+                    }
+
+                    @Override
+                    public void onActivityShow() {
+                        Log.d("========", "onActivityShow");
+                    }
+
+                    @Override
+                    public void onRewardClose() {
+                        Log.d("========", "onRewardClose");
+                    }
+
+                    @Override
+                    public void onRewardShow() {
+                        Log.d("========", "onRewardShow");
+                    }
+
+                    @Override
+                    public void onPrizeClose() {
+                        Log.d("========", "onPrizeClose");
+                    }
+
+                    @Override
+                    public void onPrizeShow() {
+                        Log.d("========", "onPrizeShow");
+                    }
+                });
                 if (ad != null) {
 //                    ad.resetSlotId(slotId+"");
                     ad.loadAd(NativeInterstitialActivity.this, false);
